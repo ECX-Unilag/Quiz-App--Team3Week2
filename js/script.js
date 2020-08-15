e2cd89104eb4aaaa1a51d0a62cb3807b3769d5b8
SirPhenom <iphenom01@gmail.com>
2020-08-15 19:01:46 +0100

    Navbar and header section of homepage

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 AOS.init();
+$(document).ready(function () {
+  $(".navbar-toggler").on("click", function () {
+    $(".animated-icon3").toggleClass("open");
+    $(".navbar").toggleClass("bg-gray");
+    $("#navbarLinks").toggleClass("bg-white");
+  });
+});
